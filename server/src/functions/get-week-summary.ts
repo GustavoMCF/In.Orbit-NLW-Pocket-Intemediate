55d65fe79142f32e586393d7cb5a30d32334ada4
Gustavo Ferreira <gustavomcferreira1@hotmail.com>
2024-09-17 11:16:59 -0300

    fix: Corrige ordenação e agregação de metas no resumo semanal
    - Corrige a ordenação das metas concluídas por data (`completedAt`) em ordem decrescente
    - Adiciona tipagem `GoalsPerDay` para garantir a correta estruturação dos dados retornados
    - Garante que as metas mais recentes apareçam primeiro utilizando `orderBy(desc(...))`

--- server/src/functions/get-week-summary.ts
+++ server/src/functions/get-week-summary.ts
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs'
 import { db } from '../db'
 import { goalCompletions, goals } from '../db/schema'
-import { and, count, eq, gte, lte, sql } from 'drizzle-orm'
+import { and, count, desc, eq, gte, lte, sql } from 'drizzle-orm'
 
 export async function getWeekSummary() {
   const firstDayOfWeek = dayjs().startOf('week').toDate()
@@ -39,6 +39,7 @@ export async function getWeekSummary() {
           lte(goalCompletions.createdAt, lastDayOfWeek)
         )
       )
+      .orderBy(desc(goalCompletions.createdAt))
   )
 
   const goalCompletedByWeekDay = db.$with('goals_completed_by_week_day').as(
@@ -57,7 +58,17 @@ export async function getWeekSummary() {
       })
       .from(goalsCompletedInWeek)
       .groupBy(goalsCompletedInWeek.completedAtDate)
+      .orderBy(desc(goalsCompletedInWeek.completedAtDate))
   )
+
+  type GoalsPerDay = Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
 
   const result = await db
     .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalCompletedByWeekDay)
@@ -70,7 +81,7 @@ export async function getWeekSummary() {
         sql /*sql*/`(SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek})`.mapWith(
           Number
         ),
-      goalsPerDay: sql /*sql*/`
+      goalsPerDay: sql /*sql*/<GoalsPerDay>`
       JSON_OBJECT_AGG(
         ${goalCompletedByWeekDay.completedAtDate},
         ${goalCompletedByWeekDay.completions}
@@ -80,6 +91,6 @@ export async function getWeekSummary() {
     .from(goalCompletedByWeekDay)
 
   return {
-    summary: result,
+    summary: result[0],
   }
 }
